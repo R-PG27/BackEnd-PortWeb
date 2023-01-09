c3ea6eba7a6453fdc89f252033b60b4c1f996753
R-PG27 <rodrigo.gormaz@mi.unc.edu.ar>
2023-01-09 19:37:12 -0300

    BackEnd.2

---
+++ src/main/java/com/portfolio/argprograma/Controller/PersonaController.java
@@ -0,0 +1,60 @@
+
+package com.portfolio.argprograma.Controller;
+
+import com.portfolio.argprograma.Entity.Persona;
+import com.portfolio.argprograma.Interface.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class PersonaController {
+    @Autowired IPersonaService ipersonaService;
+    
+    @GetMapping("phpmyadmin/persona/traer")
+    public List<Persona> getPersona(){
+    return ipersonaService.getPersona();
+    }
+    
+    @PostMapping("phpmyadmin/persona/crear")
+    public String createPersona(@RequestBody Persona persona){
+        ipersonaService.savePersona(persona);
+        return "La persona fue creada exitosamente";
+    }
+    
+    @DeleteMapping("phpmyadmin/persona/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        ipersonaService.deletePersona(id);
+        return "La persona fue eliminada exitosamente";
+    }
+    
+    @PutMapping("phpmyadmin/persona/editar/{id}")
+    public Persona editPersona(@PathVariable Long id,
+                                @RequestParam("nombre") String nuevoNombre,
+                                @RequestParam("apellido") String nuevoApellido,
+                                @RequestParam("img") String nuevoImg){
+        Persona persona = ipersonaService.findPersona(id);
+        
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImg(nuevoImg);
+        
+        ipersonaService.savePersona(persona);
+        return persona;
+    }
+
+    @GetMapping("phpmyadmin/persona/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
+    }
+
+}
