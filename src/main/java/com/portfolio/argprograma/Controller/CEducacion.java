5f10da4f254c58a8581065699c9dcc7f2abce385
R-PG27 <rodrigo.gormaz@mi.unc.edu.ar>
2023-01-17 21:32:06 -0300

    BackEnd.8

--- src/main/java/com/portfolio/argprograma/Controller/CEducacion.java
+++ src/main/java/com/portfolio/argprograma/Controller/CEducacion.java
@@ -32,7 +32,7 @@ public class CEducacion {
     @Autowired
     SEducacion sEducacion;
     
-    @GetMapping("/lista")
+    @GetMapping("phpmyadmin/lista")
     public ResponseEntity<List<Educacion>> list(){
         List<Educacion> list = sEducacion.list();
         return new ResponseEntity(list, HttpStatus.OK);
@@ -48,7 +48,7 @@ public class CEducacion {
         return new ResponseEntity(educacion, HttpStatus.OK);
     }
     
-    @PostMapping("/create")
+    @PostMapping("phpmyadmin/create")
     public ResponseEntity<?> create(@RequestBody dtoEducacion dtoedu){
         if(StringUtils.isBlank(dtoedu.getNombreEdu()))
             return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
@@ -60,7 +60,7 @@ public class CEducacion {
         
         return new ResponseEntity(new Mensaje("Educación agregada"), HttpStatus.OK);
     }
-    @PutMapping("/update/{id}")
+    @PutMapping("phpmyadmin/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoEducacion dtoedu){
         if(!sEducacion.existsById(id))
             return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
@@ -81,7 +81,7 @@ public class CEducacion {
         
     }
     
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("phpmyadmin/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         if(!sEducacion.existsById(id))
             return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
